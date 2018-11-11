7504abcd9da802b74b4ea2a029c7a27ffce97a4b
Savannah  Dutcher <savannahdutcher@wireless-nat-inside.uiowa.edu>
2018-11-11 14:47:46 -0600

    Initial Commit

---
+++ Super Cool 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Super Cool 2
+//
+//  Created by Savannah  Dutcher on 11/11/18.
+//  Copyright © 2018 Phoenix Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
